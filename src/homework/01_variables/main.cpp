92236906141c1629c53aa185709f18dd228413c5
John Bosco <boscojohn28@gmail.com>
2020-09-18 13:26:49 -0500

    Restaraunt bill

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,15 +1,39 @@
 ﻿//write include statements
-
+#include "variables.h"
 
 //write namespace using statement for cout
+#include <iostream>
 
+using std::cout; using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	double meal_amount = 0;
+	double tip_rate = 0.0;
+	double tip_amount;
+	double tax_amount;
+	double total = 0;
+
+	cout<<"Enter meal amount: ";
+	cin>>meal_amount;
+	get_sales_tax_amount(meal_amount);
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+	cout<<"\n Enter tip rate (Enter percentage as decimal): ";
+	cin>>tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout<<"\n Meal Amount: "<<meal_amount;
+	cout<<"\n Sales Tax: "<<tax_amount;
+	cout<<"\n Tip amount: "<<tip_amount;
+	cout<<"\n Total: "<<total;
 
 	return 0;
 }
+
--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -1,5 +1,4 @@
 #include "variables.h"
-
 //example
 int add_numbers(int num1, int num2)
 {
@@ -8,7 +7,24 @@ int add_numbers(int num1, int num2)
 
 //write code for multiply_numbers function here
 //function returns product of num1 and num2
+const double tax_rate = 0.0675;
+double meal_amount;
 
+double get_sales_tax_amount(double meal_amount)
+{
+	double product = 0;
+	product = tax_rate * meal_amount;
+	return product;
+}
+
+double tip_rate;
+
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	double product = 0;
+	product = tip_rate * meal_amount;
+	return product;
+}
 
 
 
