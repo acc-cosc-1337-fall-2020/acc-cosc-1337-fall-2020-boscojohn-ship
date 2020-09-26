aa16bab320a9495020f2e37f04d87929a9978f21
John Bosco <boscojohn28@gmail.com>
2020-09-25 23:59:06 -0500

    commit on master

--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,15 @@
 //Write the include statement for decisions.h here
-
+#include<iostream>
+#include "decision.h"
 //Write namespace using statements for cout and cin
-
+using std::cout; using std::cin;
 int main() 
 {
-
+	int grade;
+	cout<<"Enter a number ranging from 0 - 100: ";
+	cin>>grade;
+	std::string get_letter_grade_using_if(int grade);
+	std::string get_letter_grade_using_switch(int grade);
 	return 0;
 }
 
