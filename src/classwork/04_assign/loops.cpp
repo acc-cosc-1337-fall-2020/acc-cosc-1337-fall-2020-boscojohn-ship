fbd1ae9dd869416389770769747a52041f1da3c6
John Bosco <boscojohn28@gmail.com>
2020-10-02 21:55:06 -0500

    comit on master

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,13 @@
 //cpp
+#include "loops.h"
+#include <iostream>
+
+int display_factorial(int num)
+{
+    int factorial = 0;
+    for (int i = 0; i < num; ++i)
+    {
+        factorial *= i;
+    }
+    return factorial;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,8 @@
 //write includes statements
-
+#include "loops.h"
+#include <iostream>
 //write using statements for cin and cout
-
+using std::cout; using std::cin;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +11,21 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+	char choice;
+	int value;
+	while (choice == 'y')
+	{
+		int result;
+		cout<<"Enter a positive number: ";
+		cin>>value;
+		
+		result = display_factorial(value);
+
+		cout<<"Factorial of that number is: \n"<<result<<;
+		cout<<"Enter'y' to continue: ";
+		cin>>choice;
+
+	}
 
 	return 0;
 }
