bbf1a9e7b44850f94f8fbc7adb9fc0eae7fd8217
John Bosco <boscojohn28@gmail.com>
2020-10-24 23:49:33 -0500

    'master'

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,17 @@
+#include "tic_tac_toe.h"
+#include <iostream>
+#include <string>
+using std::cout; using std::cin; using std::string;
 
 int main() 
 {
-
+	tic_tac_toe game;
+	char player = 'X';
+	while (game.check_game_full() == false)
+	{
+		game.display_board();
+		game.mark_board();
+		game.set_next_player();
+	}
 	return 0;
 }
--- test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
+++ test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
@@ -1,7 +1,26 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "tic_tac_toe.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Plays tic tac toe")
+{
+	tic_tac_toe game;
+	REQUIRE(game.start_game('X')=true);
+	REQUIRE(game.mark_board(1) == 'X');
+	REQUIRE(game.mark_board(2) == 'O');
+	REQUIRE(game.mark_board(3) == 'X');
+	REQUIRE(game.mark_board(4) == 'O');
+	REQUIRE(game.mark_board(5) == 'X');
+	REQUIRE(game.mark_board(7) == 'O');
+	REQUIRE(game.mark_board(6) == 'X');
+	REQUIRE(game.mark_board(9) == 'O');
+	REQUIRE(game.mark_board(8) == 'X');
+
+
+
 }
 
